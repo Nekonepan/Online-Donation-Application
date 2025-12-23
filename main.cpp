3390243296151e63464e67e5ecf50a8945b8d11f
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-12-23 11:53:21 +0700

    Refactor: Add donation management functions and remove unused BST and linked list files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,69 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void insertBST();
-void iniLL();
+/* ========= STRUCT BERSAMA ========= */
+struct Donation {
+    int id;
+    string name;
+    int amount;
+};
 
+/* ========= ARRAY ========= */
+void array_add(Donation d);
+void array_sort();
+void array_display();
+void array_remove(int id);
+
+/* ========= LINKED LIST ========= */
+void list_insert(Donation d);
+void list_display();
+void list_remove(int id);
+
+/* ========= STACK ========= */
+void stack_push(Donation d);
+Donation stack_pop();
+
+/* ========= QUEUE ========= */
+void queue_enqueue(Donation d);
+Donation queue_dequeue();
+
+/* ========= TREE / BST / AVL ========= */
+void bst_insert(Donation d);
+Donation* bst_search(int id);
+void bst_remove(int id);
+
+/* ========= MENU FUNCTION ========= */
+void showMenu();
+void addDonation();
+void showAllDonations();
+void sortDonations();
+void undoDonation();
+void processQueue();
+void searchDonation();
+
+/* ========= MAIN ========= */
 int main() {
-    insertBST();
-    iniLL();
+    int choice;
+
+    do {
+        showMenu();
+        cout << "Pilih menu: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: addDonation(); break;
+            case 2: showAllDonations(); break;
+            case 3: sortDonations(); break;
+            case 4: undoDonation(); break;
+            case 5: processQueue(); break;
+            case 6: searchDonation(); break;
+            case 7: cout << "Program selesai.\n"; break;
+            default: cout << "Pilihan tidak valid!\n";
+        }
+        cout << endl;
+
+    } while (choice != 7);
+
     return 0;
 }
--- src/BST.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void insertBST() {
-    cout << "INSERT BST" << endl;
-}
